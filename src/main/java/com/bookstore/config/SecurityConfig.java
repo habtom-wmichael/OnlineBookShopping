4f7bf543a05b692e7b4fbfd4cb8b4a15eafca6b4
yadu <yaduhat@gmail.com>
2020-04-08 00:43:40 -0500

    first commit

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -19,7 +19,7 @@ import com.bookstore.utility.SecurityUtility;
 @EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
-	private Environment env;
+	private Environment environment;
 	
 	@Autowired
 	private UserSecurityService userSecurityService;
